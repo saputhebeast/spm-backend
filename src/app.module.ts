460ecfe085d09c8ee9dd2091216eb02b6a4b9b77
saputhebeast <sapumalwijekoon4@gmail.com>
2023-09-03 02:15:47 +0530

    feat: check github action

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { ConfigModule } from '@nestjs/config';
-import { ErrorMiddleware } from './common/middleware';
-import { EnvironmentConfiguration } from './config';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module';
+import { UserModule } from "./user/user.module';
+import { PrismaModule } from "./prisma/prisma.module';
+import { ConfigModule } from "@nestjs/config';
+import { ErrorMiddleware } from "./common/middleware';
+import { EnvironmentConfiguration } from "./config';
 import { validationSchema } from './config/validation';
 import { HealthModule } from './health/health.module';
 import { StorageModule } from './storage/storage.module';
