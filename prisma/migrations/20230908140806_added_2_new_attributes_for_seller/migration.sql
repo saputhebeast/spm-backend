93520f07678e7fd913781d816931b3b7608c527c
saputhebeast <sapumalwijekoon4@gmail.com>
2023-09-28 09:46:53 +0530

    feat: updated attributes

--- prisma/migrations/20230908140806_added_2_new_attributes_for_seller/migration.sql
+++ prisma/migrations/20230908140806_added_2_new_attributes_for_seller/migration.sql
@@ -8,18 +8,3 @@
 -- AlterTable
 ALTER TABLE `seller` ADD COLUMN `lat` DOUBLE NOT NULL,
     ADD COLUMN `long` DOUBLE NOT NULL;
-
--- CreateTable
-CREATE TABLE `_ItemToSubscriptionBox` (
-    `A` INTEGER NOT NULL,
-    `B` INTEGER NOT NULL,
-
-    UNIQUE INDEX `_ItemToSubscriptionBox_AB_unique`(`A`, `B`),
-    INDEX `_ItemToSubscriptionBox_B_index`(`B`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `_ItemToSubscriptionBox` ADD CONSTRAINT `_ItemToSubscriptionBox_A_fkey` FOREIGN KEY (`A`) REFERENCES `item`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `_ItemToSubscriptionBox` ADD CONSTRAINT `_ItemToSubscriptionBox_B_fkey` FOREIGN KEY (`B`) REFERENCES `subscription_box`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
