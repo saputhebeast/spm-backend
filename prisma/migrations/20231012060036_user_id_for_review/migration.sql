2b60cb7f169f9c625ebaddb8884ad9750a19756b
shabinaf <shabeena.farveen@rootcodelabs.com>
2023-10-12 11:36:27 +0530

    refactor

--- prisma/migrations/20231012060036_user_id_for_review/migration.sql
+++ prisma/migrations/20231012060036_user_id_for_review/migration.sql
@@ -5,9 +5,6 @@
 
 */
 -- AlterTable
-ALTER TABLE `item` MODIFY `imageUrl` VARCHAR(191) NOT NULL DEFAULT 'https://my-se-bucket-12.s3.ap-south-1.amazonaws.com/1697081389343-e60a3c36-7449-4b67-a441-275cc34f3e13.png';
-
--- AlterTable
 ALTER TABLE `review` ADD COLUMN `userId` INTEGER NOT NULL;
 
 -- AddForeignKey
