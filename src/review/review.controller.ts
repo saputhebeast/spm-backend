3c905eaf51ff4e20b168897ebea1e904f861ab70
shabinaf <shabeena.farveen@rootcodelabs.com>
2023-10-13 23:59:36 +0530

    refactor

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -103,8 +103,9 @@ export class ReviewController {
     @GetUser('id') userId: number,
     @Res() res: Response,
   ) {
-    const data: Review[] =
-      await this.reviewService.getReviewsOfCurrentUser(userId);
+    const data: Review[] = await this.reviewService.getReviewsOfCurrentUser(
+      userId,
+    );
     return makeResponse({
       res,
       status: HttpStatus.OK,
