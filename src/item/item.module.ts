9f3902e51837872329c73deac37d3af40676f130
saputhebeast <sapumalwijekoon4@gmail.com>
2023-10-14 00:46:56 +0530

    feat: improved recommendation system using preferences

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
 import { ItemRepository } from './item.repository';
+import { PreferenceRepository } from '../preference/preference.repository';
 
 @Module({
   controllers: [ItemController],
-  providers: [ItemService, ItemRepository],
+  providers: [ItemService, ItemRepository, PreferenceRepository],
 })
 export class ItemModule {}
